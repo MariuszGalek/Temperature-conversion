32dc691967d62a416c78b9e118a391f3efe274d7
Mariusz Galek <MariuszGalek@users.noreply.github.com>
2020-04-15 20:08:48 +0200

    Add javascript variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-const 
+const convBtn = document.querySelector('.conv');
+const resetBtn = document.querySelector('.reset');
+const swapBtn = document.querySelector('.swap');
+const firstTemp = document.querySelector('.one');
+const secTemp = document.querySelector('.two');
+const number = document.querySelector('.converter');
+const result = document.querySelector('.result');
+
+let celsius;
+let fahrenheit;
+
+convBtn.addEventListener('click', function(){
+
+});
+
+resetBtn.addEventListener('click', function(){
+
+});
+
+swapBtn.addEventListener('click', function(){
+
+});
+// T(°F) = T(°C) × 1.8 + 32 
+// T(°C) = (T(°F) - 32) / 1.8
