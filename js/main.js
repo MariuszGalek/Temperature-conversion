b40455157bd69a766ea63feefead530b59fcc310
Mariusz Galek <MariuszGalek@users.noreply.github.com>
2020-04-21 18:49:50 +0200

    Add conversion function

--- js/main.js
+++ js/main.js
@@ -11,17 +11,29 @@ let fahrenheit;
 
 let celToF = () => {
     fahrenheit = number.value * 1.8 + 32;
-    result.innerText = `${number.value} °C is ${fahrenheit} °F.`;
+    result.innerText = `${number.value} °C is ${fahrenheit.toFixed(1)} °F.`;
     converter.value = '';
 };
 
 let fToCel = () => {
     celsius = (number.value - 32) / 1.8;
-    result.innerText = `${number.value} °F is ${celsius} °C.`;
+    result.innerText = `${number.value} °F is ${celsius.toFixed(1)} °C.`;
     converter.value = '';
 };
 
-
+let conversion = () => {
+    if (number.value !== '') {
+        if (firstTemp.innerText === '°C')
+        { 
+            celToF();
+        }
+        else {
+            fToCel();
+        };
+    } else {
+        result.innerText = "Enter some number!";
+    };
+};
 //resetBtn.addEventListener('click', function(){
 
 //});
@@ -30,11 +42,13 @@ let swap = () => {
     if (firstTemp.innerText === "°C") {
         firstTemp.innerText = "°F";
         secTemp.innerText = "°C";
+        result.innerText = '';
     } else {
         firstTemp.innerText = "°C";
         secTemp.innerText = "°F";
+        result.innerText = '';
     };
 };
 
-
+convBtn.addEventListener('click', conversion);
 swapBtn.addEventListener('click', swap);
